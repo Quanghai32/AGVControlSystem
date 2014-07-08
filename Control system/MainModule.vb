e940f9a619bc7e47c3abdce1531c272bdaf5dec4
Coffee Den <cvn>
2014-07-08 10:21:35 +0700

    Fix error when don't have COM port

--- Control system/MainModule.vb
+++ Control system/MainModule.vb
@@ -7,23 +7,25 @@ Public Module MainModule
         Dim list() As String
         list = New String(HostXbee.Length - 1) {}
         Dim tempXbee As XBee = New XBee
-        For i As Byte = 0 To HostXbeeName.Length - 1
-            tempXbee.SettingPort(HostXbeeName(i), 9600)
-            tempXbee.Address = 0
-            tempXbee.Send_AT_Command(XBee.AT_COMMAND_ENUM.SERIAL_NUMBER_LOW)
-            Dim CurrentTime As Integer = Environment.TickCount
-            While (tempXbee.Address = 0) And (CurrentTime + 1000) > Environment.TickCount
-            End While
-            If tempXbee.Address <> 0 Then
-                For j As Byte = 0 To HostXbee.Length - 1
-                    If tempXbee.Address = HostXbee(j).Address Then
-                        list(j) = HostXbeeName(i)
-                        Exit For
-                    End If
-                Next
-            End If
-            tempXbee.ClosePort()
-        Next
+        If HostXbeeName.Length > 0 Then
+            For i As Byte = 0 To HostXbeeName.Length - 1
+                tempXbee.SettingPort(HostXbeeName(i), 9600)
+                tempXbee.Address = 0
+                tempXbee.Send_AT_Command(XBee.AT_COMMAND_ENUM.SERIAL_NUMBER_LOW)
+                Dim CurrentTime As Integer = Environment.TickCount
+                While (tempXbee.Address = 0) And (CurrentTime + 1000) > Environment.TickCount
+                End While
+                If tempXbee.Address <> 0 Then
+                    For j As Byte = 0 To HostXbee.Length - 1
+                        If tempXbee.Address = HostXbee(j).Address Then
+                            list(j) = HostXbeeName(i)
+                            Exit For
+                        End If
+                    Next
+                End If
+                tempXbee.ClosePort()
+            Next
+        End If
         Dim Str As String = New String("Can not connect with ")
         Dim isError As Boolean = False
         For i As Byte = 0 To list.Length - 1
